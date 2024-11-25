80cef5d8e96ff328192e60c039096e989be73e4f
Cecilia Inoa <300139282@monboreal.ca>
2024-11-24 21:47:57 -0500

    Ajout de methode pour faire la conversion des differents temperatures, modificaions des noms boutons

--- MauiAppConvertisseur/MainPage.xaml.cs
+++ MauiAppConvertisseur/MainPage.xaml.cs
@@ -2,31 +2,56 @@
 {
     public partial class MainPage : ContentPage
     {
-        //int count = 0;
+
 
         public MainPage()
         {
             InitializeComponent();
         }
 
-        /*private void OnCounterClicked(object sender, EventArgs e)
-        {
-            count++;
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
+       private double Button_Clicked_CF(double celsius)  //conversion de C a F
+        {
+            return (celsius * 9 / 5) + 32;
+        }
+
+        private double Button_Clicked_FC(double fahrenheit) //conversion de  F a C 
+        {
+            
+            return (fahrenheit-32)*  5/9;
+        }
+
+        private void Button_Clicked_CF(object sender, EventArgs e)
+        {
+            if (double.TryParse(celEntry.Text, out double celsius))
+            {
+
+                double fahrenheit = Button_Clicked_CF(celsius ) ; 
+                celEntry.Text = fahrenheit.ToString("F2");
+            }
+
             else
-                CounterBtn.Text = $"Clicked {count} times";
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
-        }*/
-
-        private async void Button_Clicked(object sender, EventArgs e)
+            {
+                DisplayAlert("Ce n'est pas une valeur valide", "Veuillez entrer une valeur valide pour Celsius.", "OK");
+            }
+        }
+        private void Button_Clicked_FC(object sender, EventArgs e)
         {
+            if  (double.TryParse(fahEntry.Text, out double fahrenheit))
 
-            await Navigation.PopAsync();
 
+            {
+                double celsius = Button_Clicked_FC(fahrenheit) ;
+                fahEntry.Text = celsius.ToString("F2");
+            }
+
+            else
+            {
+                DisplayAlert("Ce n'est pas une valeur valide", "Veuillez entrer une valeur valide pour Fahrenheit.", "OK");
+            }
         }
 
     }
-}
+
+}   
